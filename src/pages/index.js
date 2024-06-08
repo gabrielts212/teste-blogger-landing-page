814a6bf223bf10377d774a890f2ce77c3cd1a3eb
gabrielts212 <gabrielts2122@gmail.com>
2024-06-08 16:56:40 -0300

    👍

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import Footer from "../components/footer/footer";
-import Header from "../components/header/header";
-import Contact from "../components/contact/contact";
-import Plans from "../components/plans/plans";
-import Form from "../components/form/form";
-import Business from "../components/business/business";
-import GreatWork from "../components/greatwork/greatwork";
-import Principalpage from "../components/principalpage/principalpage";
+import Footer from "@/components/footer/footer";
+import Header from "@/components/header/header";
+import Contact from "@/components/contact/contact";
+import Plans from "@/components/plans/plans";
+import Form from "@/components/form/form";
+import Business from "@/components/business/business";
+import GreatWork from "@/components/greatwork/greatwork";
+import Principalpage from "@/components/principalpage/principalpage";
 
 export default function Home() {
   return (
